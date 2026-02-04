4c26a01851a7f5e5b28c01aa8d6a76be19780730
abdogaber10 <hazogaber10@gmail.com>
2026-02-04 19:00:14 +0200

    GenericRepository

---
+++ Core/Domain/Contracs/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using Domain.Entities;
+
+namespace Domain.Contracs
+{
+    public interface IGenericRepository<TEntity , TKey> where TEntity : BaseEntity<TKey>
+    {
+        // GetAll 
+        Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = false);
+
+        // GetById
+        Task<TEntity?> GetByIdAsync(TKey id);
+
+        // Add 
+        Task AddAsync(TEntity entity);
+
+        // Update
+        void Update(TEntity entity);
+
+        // Delete 
+        void Delete(TEntity entity);
+    }
+}
---
+++ Infrastructure/Presistence/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Contracs;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Presistence.Data.Dbcontexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _dbContext;
+        public GenericRepository( StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEntity entity)
+           => await _dbContext.AddAsync(entity);
+
+        public void Delete(TEntity entity)
+             =>  _dbContext.Set<TEntity>().Remove(entity);
+
+        // Delete OR Update OR Add دي مش بتخليها تتبع التغيرات ولا تتبع البيانات اللي جايه من الداتا بيز ومش هيعدل من هنا لازم لو هيعدل يعدل من  asNoTracking
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = false)
+          => asNoTracking ? await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync() 
+            : await _dbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+              => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public void Update(TEntity entity)
+             => _dbContext.Set<TEntity>().Update(entity);
+    }
+}
