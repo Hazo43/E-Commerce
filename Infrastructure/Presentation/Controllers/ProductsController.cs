288fc2f341b4a25de0407b330e26174a010f0cb5
abdogaber10 <hazogaber10@gmail.com>
2026-02-13 00:40:22 +0200

    Product Controller

---
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions.Contracts;
+using Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+ 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        public ProductsController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+        // EndPoint => Get AllProduct 
+        // Get : BaseUrl/api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductResultDto>>> GetAllProductAsync()
+        {
+            var products = await _serviceManager.ProductService.GetAllProductsAsync();
+            return Ok(products);
+        }
+        // EndPoint => Get AllBrands
+        // Get : BaseUrl/api/Products/Brands
+        [HttpGet("Brands")]
+        public async Task<ActionResult<IEnumerable<BrandResultDto>>> GetAllBrandsAsync()
+        {
+            var brands = await _serviceManager.ProductService.GetAllBrandsAsync();
+            return Ok(brands);
+        }
+        // EndPoint => Get AllTypes
+        // Get : BaseUrl/api/Products/Types
+        [HttpGet("Types")]
+        public async Task<ActionResult<IEnumerable<TypeResultDto>>> GetAllTypesAsync()
+        {
+            var types = await _serviceManager.ProductService.GetAllTypesAsync();
+            return Ok(types);
+        }
+        [HttpGet("{id:int}")]
+        // EndPoint => Get ProductById
+        // Get : BaseUrl/api/Products/Id
+        public async Task<ActionResult<ProductResultDto>> GetProductByIdAsync(int id)
+        {
+            var product = await _serviceManager.ProductService.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+
+    }
+}
