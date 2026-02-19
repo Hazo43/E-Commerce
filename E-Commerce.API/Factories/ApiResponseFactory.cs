7075d854fc50e60691292d160c0ad882b06ce0ea
abdogaber10 <hazogaber10@gmail.com>
2026-02-19 03:11:02 +0200

    Handling Validation Error Response

---
+++ E-Commerce.API/Factories/ApiResponseFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.ErrorModels;
+
+
+
+namespace E_Commerce.API.Factories
+{
+    public class ApiResponseFactory
+    {
+        public static IActionResult CustomValidationErrorResponse(ActionContext context)
+        {
+            //Context ==> errors , Key [Faild]
+            // context.modelstate ==> <string , modelstateEntry>
+            // string ==> name of the faild   اللي حصل فيه الايرور faild دي ال
+            // modelstateEntry ==> Errors ==> Error Message 
+
+            //  ValidationError في ال Select هروح اعملو  error لو فيه اي  ModelState في ال errors بتفحص بيشوف فيه اي
+            var errors = context.ModelState
+                .Where(error => error.Value?.Errors.Any() == true).Select(error => new ValidationError()
+                {
+                    Field = error.Key,
+                    Errors = error.Value.Errors.Select(error => error.ErrorMessage)
+                });
+
+            // ValidationError دا شكل الداتا اللي هترجع لو حصل اي
+            var response = new ValidationErrorResponse()
+            {
+                Errors = errors,
+                StatusCode = StatusCodes.Status400BadRequest,
+                ErrorMessage = "One Or More Validation Error happened",
+            };
+            
+            return new BadRequestObjectResult(response);
+
+        }
+    }
+}
--- E-Commerce.API/Program.cs
+++ E-Commerce.API/Program.cs
@@ -1,6 +1,8 @@
 
 using Domain.Contracs;
+using E_Commerce.API.Factories;
 using E_Commerce.API.MiddleWares;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Presistence.Data.DataSeed;
 using Presistence.Data.Dbcontexts;
@@ -25,7 +27,11 @@ namespace E_Commerce.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            // 
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = ApiResponseFactory.CustomValidationErrorResponse;
+            });
             builder.Services.AddDbContext<StoreDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
