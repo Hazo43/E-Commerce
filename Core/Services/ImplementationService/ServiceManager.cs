97e06e93fe2969c65f9ea1122abe59aafbf34892
abdogaber10 <hazogaber10@gmail.com>
2026-02-21 15:36:03 +0200

    Authentication Controller

--- Core/Services/ImplementationService/ServiceManager.cs
+++ Core/Services/ImplementationService/ServiceManager.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Domain.Contracs;
+using Domain.Entities.IdentityModule;
+using Microsoft.AspNetCore.Identity;
 using Services.Abstractions.Contracts;
 using System;
 using System.Collections.Generic;
@@ -9,13 +11,19 @@ using System.Threading.Tasks;
 
 namespace Services.ImplementationService
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper , IBasketRepository _basketRepository) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, 
+                               IMapper _mapper ,
+                               IBasketRepository _basketRepository , 
+                               UserManager<User> _userManager) : IServiceManager
     {
 
         private readonly Lazy<IProductService> _productService = new Lazy<IProductService>(() => new ProductService(_unitOfWork , _mapper));
         private readonly Lazy<IBasketService> _basketService = new Lazy<IBasketService>(() => new BasketService(_basketRepository, _mapper));
-       
+        private readonly Lazy<IAuthenticationService> _authService = new Lazy<IAuthenticationService>(() => new AuthenticationService(_userManager));
         public IProductService ProductService =>_productService.Value;
+       
         public IBasketService BasketService =>_basketService.Value;
+        
+        public IAuthenticationService AuthenticationService => _authService.Value;
     }
 }
---
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions.Contracts;
+using Shared.DTOs.IdentityModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    public class AuthenticationController : ApiController
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public AuthenticationController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        // Post ==> Login
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserResultDto>> LoginAsync(LoginDTO loginDTO)
+        {
+            var login = await _serviceManager.AuthenticationService.LoginAsync(loginDTO);
+            return Ok(login);
+        }
+
+        // Post => Register
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserResultDto>> RegisterAsync (RegisterDTO registerDTO)
+        {
+            var register = await _serviceManager.AuthenticationService.RegisterAsync(registerDTO);
+            return Ok(register);
+        }
+    }
+}
