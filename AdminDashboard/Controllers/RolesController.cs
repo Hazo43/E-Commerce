8be96944cd13fac5856a2f9fe77b8276d935e30e
abdogaber10 <hazogaber10@gmail.com>
2026-03-27 01:15:16 +0200

    AdminDashboard -> 02 Managing roles [Index, Create] actions

---
+++ AdminDashboard/Controllers/RolesController.cs
@@ -0,0 +1,50 @@
+﻿using AdminDashboard.Models.Roles;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AdminDashboard.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        public async Task<IActionResult> Create(RoleViewModel model)
+        {
+            //1- ميسبوش فاضي name متاحه ب الشروط اللي ان حاطتها متكونش اكتر من256 حرف و يبعت ال ModelState في حاله ال
+            if ( ModelState.IsValid )
+            {
+               // 1.1- الاول Role بنجيب ال
+                var roleExist = await _roleManager.RoleExistsAsync(model.Name);
+
+                //1.2- Role مش موجوده هضيف ال Role لو ال
+                if ( !roleExist )
+                {
+                    // اللي هو عاوز الاسم بتاعها IdentityRole علي حسب ال Role هضيف 
+                    await _roleManager.CreateAsync(new IdentityRole(model.Name));
+                   
+                    // Index اللي في ال Roles  بعد ما تضيف و تخلص روح اعرضلي كل الRole
+                    return RedirectToAction(nameof(Index));
+                }
+                //1.3- موجوده role ان ال Error موجوده هروح ارجعلو roleExist لو بقا ال
+
+                ModelState.AddModelError("Name", " Name Already Exists");
+            }
+
+            //2- name more than 256 char او بعت name في حاله انو مبعتش ال Index هرجعو ع ال
+            //      اللي كانت موجوده قبل كدا  Roles و رجعو ع ال
+            return View(nameof(Index), await _roleManager.Roles.ToListAsync());
+        }
+    }
+}
---
+++ AdminDashboard/Models/Roles/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminDashboard.Models.Roles
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage = " Role Name Is Required")]
+        [StringLength( 256 , ErrorMessage = " Role Name Size Can't Be More Than 256 Char. ")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
