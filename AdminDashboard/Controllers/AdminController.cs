a3c6cb20295b1aff10d442dc21cbb1e3dd224fe4
abdogaber10 <hazogaber10@gmail.com>
2026-04-02 22:57:50 +0200

    Admin Dashboard => 05 Admin controller [Login, Logout] actions

---
+++ AdminDashboard/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Entities.IdentityModule;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs.IdentityModule;
+
+namespace AdminDashboard.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AdminController(UserManager<User> userManager , SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDTO.Email);
+
+            // DB م موجود عندي في ال user لو ال
+            if ( user is null)
+            {
+                ModelState.AddModelError("", "Invalid Login attempt");
+                return View(loginDTO);
+            }
+            // عشان نشوف هو دخلو صح ولا لا password هنجيب ال
+            var password = await _signInManager.PasswordSignInAsync(user, loginDTO.Password, false, false);
+
+            //  (Admin Or SuperAdmin) ل role اللي انت دخلتو غلط او انت معندكش password لو ال
+            if (!password.Succeeded || (!await _userManager.IsInRoleAsync(user, "Admin") && (!await _userManager.IsInRoleAsync(user, "SuperAdmin"))))
+            {
+                ModelState.AddModelError("", "You are not authoruzed");
+                return View(loginDTO);
+            }
+
+            // (Admin Or SuperAdmin) ل Role صح و عندو password لو هو مدخل ال
+            // Home بتاع ال Controller في ال Index هرجعو علي ال
+            return RedirectToAction(nameof(Index) , "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
--- AdminDashboard/Program.cs
+++ AdminDashboard/Program.cs
@@ -39,14 +39,14 @@ namespace AdminDashboard
                 options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
             });
             // User Identity Role 
-            builder.Services.AddIdentityCore<User>(options =>
+            builder.Services.AddIdentity<User , IdentityRole>(options =>
             {
                 options.Password.RequireNonAlphanumeric = true;
                 options.Password.RequireDigit = true;
                 options.Password.RequireLowercase = true;
                 options.Password.RequireUppercase = true;
                 options.User.RequireUniqueEmail = true;
-            }).AddRoles<IdentityRole>().AddEntityFrameworkStores<IdentityStoreDbContext>();
+            }).AddEntityFrameworkStores<IdentityStoreDbContext>().AddDefaultTokenProviders();
 
             var app = builder.Build();
 
@@ -67,90 +67,10 @@ namespace AdminDashboard
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Admin}/{action=Login}/{id?}");
 
             app.Run();
         }
-//        1- Project setup
-//-- add project mvc named adminDashboard in api project solution
-//-- install dashboard template
-//-- add project reference from admindashboard to talabat.APIs
-//-- copy connection string from api and add it in admindashboard without redis
-//-- in talabat api in the startup copy the part of configure db contexts and add them in mvc project 
-//-- in talabat api copy the part of identity from identityserviceextension and add it in the startup
-//-- in admindashboard in layout add the links for css , js..and add them in wwwroot
-//-- add the nav bar in the layout and remove the search
-//-- add the side bar in the layout, remove everything inside layoutSidenav_content div and add the div container inside it
-//-- remove the footer and uncessary things
-//-- in sidebar : 
-//-- add new div Secuirty and make asp-controller = Role , asp-action = index , change icon to roles and dashboard word to roles
-//-- add new link Users and asp-controller = user and asp-action = Index
-//*****************************************************************************************************************
-                          //2- ManageRoles 
-//-- add RoleController, inject RoleManager
-//-- implement index in role controller
-//-- add view to index 
-//-- add new vm RoleFormViewModel
-//-- add RoleFormPartialView in views
-//-- render the partial view in the index page 
-//-- in controller add the create action , delete action
-//-- add new vm RoleViewModel
-//-- in controller add the edit action 
-//-- add view for edit 
-//-- in controller add the edit post action 
-//*****************************************************************************************************************
-                                 //3- Manage users
-//-- add user controller
-//-- create userViewModel
-//-- create index action
-//-- create index view
-//-- add UserRoleViewModel
-//-- add edit action get
-//-- add edit view
-//-- add edit action post
-//*****************************************************************************************************************
-                               //4- Login/Logout
-//-- add controller AdminController
-//-- add login action get  in controller
-//-- add login view
-//-- edit code of template and add aspaction in the method
-//-- edit route in startup
-//-- add login action post in controller
-//-- add logout action in controller
-//-- add asp-action and asp-controller for logout in the layout 
-//*****************************************************************************************************************
-                              //5- Manage product 
-//-- in layout add another part for Products and brands
-//-- add new controller ProductController
-//-- add ProductViewModel
-//-- add new folder Helpers
-//-- add new class in helpers , MapsProfile include mapps profile
-//-- in startup, allow dependency injection for Iunit of work , Maps profile
-//-- add the index action to get all products
-//-- add the view that include all the products
-//-- copy images folder from wwwroot in api project to my project
-//-- in helpers add pictureSetting class
-//-- add brandConfig in Talabat.Repository
-//-- add migration the default pro is talabat.repository 
-//-- in product controller add create action[get]
-//-- add partialview called CreatEditPartialView, inject in it IUnitOfWork, Brands, Types
-//-- add create product view 
-//-- add create action[post]
-//-- add edit action in the productcontroller[get]
-//-- change create view to render partial view
-//-- add edit view
-//-- add edit action[post]
-//-- add delete action[get]
-//-- add delete view
-//-- add delete action[post]
-//*****************************************************************************************************************
-                              //6- Manage Brands
-//-- add BrandsController
-//-- add index action 
-//-- add index view
-//-- add create action 
-//-- add CreateFormPartialView 
-//-- add delete action
-
+     
     }
 }
